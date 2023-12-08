b154a938d5182eaa2d417d2584d8d4f11d21a692
sio1 <troubadour518@gmail.com>
2023-12-08 17:09:08 +0100

    début du projet/début de la 1ère page.

---
+++ accueil.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+
+  <title>Bibliothèque</title>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <link rel="stylesheet" type="text/css" href="style.css"/> -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+</head>
+
+
+
+<body>
+
+
+
+
+
+
+<div class="container">
+
+
+
+<div class="row">
+  <div class="col-md-12">
+<?php include 'entete.html';?>
+  </div>
+</div>
+
+<div class="row">
+
+
+
+<div class="col-md-8">carroussel</div>
+
+<div class="col-md-4">
+<?php include 'authentification.php';?>
+</div>
+
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+  <?php
+
+  // Commentaire sur une ligne
+  
+  /*
+  Sur Plusieurs lignes
+  */
+
+  // On se  connecte, voir code du fichier connexion.php ci-dessus
+  
+  
+
+
+  
+
+  ?>
+
+
+
+
+
+
+
+</body>
---
+++ authentification.php
@@ -0,0 +1,3 @@
+<?php
+ echo "vide temporairement"
+?>
