615e825ff20524128782e04715a5ff5b03b86ee1
gaelann le cozleer <troubadour518@gmail.com>
2023-12-16 14:33:37 +0100

    Changement border sur le formulaire page d'accueil

--- authentification.php
+++ authentification.php
@@ -1,24 +1,24 @@
 <div class="container pt-5">
     <div class="d-flex justify-content-center">
-    <span class="border">
-        <form name="nameForm" method="post" action="traitement.php">
-            Se connecter 
-            <br>
-            <br>
-            Identifiant 
-            <br>
-            
-            <input type="text" name="nom"> 
-            <br>
-            <br>
-            Mot de passe 
-            <br>
-            
-            <input type="text" name="prenom">
-            <br>
-            <br>
-            <input type="submit" value="Connexion">
-        </form>
+        <span class="border border-5 border-dark">
+            <form name="nameForm" method="post" action="traitement.php">
+                Se connecter 
+                <br>
+                <br>
+                Identifiant 
+                <br>
+                
+                <input type="text" name="nom"> 
+                <br>
+                <br>
+                Mot de passe 
+                <br>
+                
+                <input type="text" name="prenom">
+                <br>
+                <br>
+                <input type="submit" value="Connexion">
+            </form>
         </span>
     </div>
 </div>
